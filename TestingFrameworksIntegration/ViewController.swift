d6a821a5a1a8fa2b3aaa93de831da91e87ccfece
imostoviy <i_mostoviy@ukr.net>
2020-06-25 21:54:20 +0300

    Initial Commit

---
+++ TestingFrameworksIntegration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingFrameworksIntegration
+//
+//  Created by Ihor Mostovyi on 25.06.2020.
+//  Copyright © 2020 Ihor Mostovyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
